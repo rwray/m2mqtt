0964a292828efbb248b08bff52e6c71677e1ecc7
rwray <ryan@zeto.ie>
2015-12-03 16:49:09 +0000

    Basic changes to allow .NET 4.0 target

--- M2Mqtt/Net/MqttNetworkChannel.cs
+++ M2Mqtt/Net/MqttNetworkChannel.cs
@@ -420,7 +420,23 @@ namespace uPLibrary.Networking.M2Mqtt
     /// </summary>
     public static class MqttSslUtility
     {
-#if (!MF_FRAMEWORK_VERSION_V4_2 && !MF_FRAMEWORK_VERSION_V4_3 && !COMPACT_FRAMEWORK)
+
+#if (FRAMEWORK_VERSION_V4_0)
+		public static SslProtocols ToSslPlatformEnum(MqttSslProtocols mqttSslProtocol)
+		{
+			switch (mqttSslProtocol)
+			{
+			case MqttSslProtocols.None:
+				return SslProtocols.None;
+			case MqttSslProtocols.SSLv3:
+				return SslProtocols.Ssl3;
+			case MqttSslProtocols.TLSv1_0:
+				return SslProtocols.Tls;
+			default:
+				throw new ArgumentException("SSL/TLS protocol version not supported");
+			}
+		}
+#elif (!MF_FRAMEWORK_VERSION_V4_2 && !MF_FRAMEWORK_VERSION_V4_3 && !COMPACT_FRAMEWORK)
         public static SslProtocols ToSslPlatformEnum(MqttSslProtocols mqttSslProtocol)
         {
             switch (mqttSslProtocol)
